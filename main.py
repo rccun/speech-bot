6b3b079544a69b72a1c8525384328386ca6f284e
Karina <gorskinakarina@gmail.com>
2025-09-19 12:37:21 +0500

    изменена структура кода, доработано блокирование пользователя на пять минут, ведется учет количества предупреждений. изменен список слов

--- main.py
+++ main.py
@@ -14,70 +14,86 @@ import logging
 from datetime import datetime, timedelta
 import json
 
-def load_data():
-    with open('alerts_doc.json', 'w', encoding='utf-8') as f:
-        alerts = json.load(f)
-    with open('blacklist.txt', 'w', encoding='utf-8') as f:
-        blacklist = f.readline()
+async def download_data():
+    with open('alerts_doc.txt', 'r', encoding='utf-8') as f:
+        return f.read()
+
+
+async def upload_data():
+    with open('alerts_doc.txt', 'w', encoding='utf-8') as f:
+        for i, j in alerts.items():
+            print(f'{i}:{j}')
+            f.write(f'{i}:{j}')
+
+
+def convert_toDict(alerts_txt):
+    global alerts
+    for i in alerts_txt.split('\n'):
+        if len(i) != 0:
+            alerts[int(i[:i.find(':')])] = int(i[i.find(':') + 1:])
 alerts = {}
-blacklist = []
+admins = [1876535694]#1278617806, 1876535694]
 group_id = 0
 c = 0
-ids = []
-answer = ''
+usid = 0
 usname = ''
 k = ''
+group_id = -1002970117013
+words = ['ебло', 'еблан', 'мудак', 'пидор', 'пидрила', 'захуячу', 'хуета']
+shorts = ['бля', 'пизд', 'хуй', 'еба', 'сос', 'хуе']
+isLast = False
+
 bot = Bot(token=apiska)
 dp = Dispatcher(bot)#, storage=MemoryStorage())
 logging.basicConfig(level=logging.INFO)
-# @dp.message_handler(commands='start')
-# async def start(mes: types.Message):
-#    group_id = mes.chat.id
-   
-#    await mes.answer("Здравствуйте сучки")
-    #await bot.send_message(mes.from_user.id,
-          #                 'Привет! тут чтото должно быыть написано типа приветствия и рассказа про кафе',
-           #                reply_markup=kb)
+@dp.message_handler(commands='start')
+async def start(mes: types.Message):
+    await bot.send_message("Это бот для фильтрации нецензурных слов в группах. Добавьте меня в группу, выдайте права администратора и я буду удалять сообщения, содержащие нецензурные слова")
 
 @dp.message_handler()
-async def start(mes: types.Message):
-    #load_data()
-    print(mes.chat.id)
-    print(mes.from_user.id)
-    group_id = -1002970117013
-    words = ['бля', 'блят', 'хуй', 'пизда', 'ебло', 'хуйло', 'еблан', 'хуесос ', 'мудак', 'пидор', 'пидрила', 'заебала', 'захуячу', 'заебашу', 'соси', 'нахуй ', 'похуй']
-    shorts = ['бля', 'пизд', 'хуй', 'еба']
+async def filter(mes: types.Message):
+    global alerts
+    alerts.clear()
+    convert_toDict(await download_data())
+    print("ID чата", mes.chat.id)
+    print("ID пользака", mes.from_user.id)
+    
     list_words = mes.text.split()
     #if mes.chat.id != -1002970117013:
-    if mes.from_user.id != 866244297: 
+    if mes.from_user.id not in admins:
         for k in list_words:
             i = k.lower()
             for j in shorts:
-                if i in words or j in i: 
+                if i in words or j in i:
                     usname = mes.from_user.username
-                    if usname in alerts.keys():
-                        c = (alerts[usname] + 1) % 3
-                    else: c = 1
-                    if c == 0: 
-                        await mes.reply(f'Последнее предупреждение. Бан @{usname}')
-                        until_date = datetime.now() + timedelta(minutes=2)
-                        blacklist.append(usname)
+                    if usname == None:
+                        usname = mes.from_user.first_name
+                    else: 
+                        usname = '@' + mes.from_user.username
+                    usid = mes.from_user.id
+                    await bot.send_message(group_id, f'в группу {mes.chat.full_name} пидорас {usname} отправил такую хуету: {mes.text}')
+                    if usid in alerts.keys():
+                        count = (alerts[usid] + 1) % 3
+                    else:
+                        count = 1
+                    alerts[usid] = count
+                    if count == 0:
+                        until_date = datetime.now() + timedelta(minutes=5)
+
+                        await mes.reply(f'Последнее предупреждение. Бан {usname} на 5 минут')
+                        await upload_data()
                         await bot.restrict_chat_member(chat_id=mes.chat.id, user_id=mes.from_user.id, until_date=until_date)
                         await mes.delete()
                         break
-                    alerts[usname] = c
-                    await mes.reply(f"@{usname} не матерись пжлст\nПредупреждение: {c}/3")
-                    await bot.send_message(group_id, f'в группу {mes.chat.full_name} пидорас @{usname} отправил такую хуету: {mes.text}')
+                    await bot.send_message(mes.chat.id, f"{usname} Без мата! Мир дружба жвачка\nПредупреждение: {count}/3")
                     await mes.delete()
-    else: await mes.reply("вероника лошаааааааааааааааааааара")
-    # else: 
-        #     await mes.reply(f'привет @rccun')
-        #     print("ldjfsglkdfjglkfjglsd")
+                    await upload_data()
+                    break
 
+    
 
-
-
-
+    
+            
 
 
 
--- test.py
+++ test.py
@@ -1,10 +1,6 @@
-# import sys
+import sys
 
-# print("Введите многострочный текст (Ctrl+D/Ctrl+Z для завершения):")
-# multiline_input = sys.stdin.read()
-# print(multiline_input.split('\n'))
+print("Введите многострочный текст (Ctrl+D/Ctrl+Z для завершения):")
+multiline_input = sys.stdin.read()
+print(multiline_input.split('\n'))
 
-alerts = {'dfgfg' : 3}
-c = 0
-c += alerts['dfgfg'] % 3
-print(c)
